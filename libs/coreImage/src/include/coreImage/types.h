36fd7b0846526673f752639d57df700967111fe8
Vadim Yarmushov <vad_911@mail.ru>
2026-01-25 17:44:59 +0300

    minor edit  version lib  v 0.0.2

--- libs/coreImage/src/include/coreImage/types.h
+++ libs/coreImage/src/include/coreImage/types.h
@@ -54,4 +54,44 @@ namespace myCoreImage
         bool isInteger() const noexcept;
         bool isSigned()  const noexcept;
     };
-}
+
+    struct ChannelElementDesc
+{
+    ChannelDataType dataType{};
+    ChannelBitDepth bitDepth{};
+
+    constexpr bool isValid() const noexcept
+    {
+        if (dataType == ChannelDataType::Float)
+        {
+            return bitDepth == ChannelBitDepth::Bit16 ||
+                   bitDepth == ChannelBitDepth::Bit32 ||
+                   bitDepth == ChannelBitDepth::Bit64;
+        }
+
+        if (dataType == ChannelDataType::UnsignedInt)
+        {
+            return bitDepth == ChannelBitDepth::Bit1 ||
+                   bitDepth == ChannelBitDepth::Bit8 ||
+                   bitDepth == ChannelBitDepth::Bit16 ||
+                   bitDepth == ChannelBitDepth::Bit32 ||
+                   bitDepth == ChannelBitDepth::Bit64;
+        }
+
+        if (dataType == ChannelDataType::SignedInt)
+        {
+            return bitDepth == ChannelBitDepth::Bit8 ||
+                   bitDepth == ChannelBitDepth::Bit16 ||
+                   bitDepth == ChannelBitDepth::Bit32 ||
+                   bitDepth == ChannelBitDepth::Bit64;
+        }
+
+        return false;
+    }
+
+    constexpr std::size_t bytesPerElement() const noexcept
+    {
+        return static_cast<std::size_t>(bitDepth) / 8;
+    }
+};  // -- end struct ChannelElementDesc
+}   // -- end namespace myCoreImage
